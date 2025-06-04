694357d21c40fdc484b54d4893c2971da4feb88c
Layalia Nur Afifah <layalia.alia.10@gmail.com>
2025-06-04 21:18:37 +0700

    Langkah ketiga deployment

--- api/index.js
+++ api/index.js
@@ -5,6 +5,9 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 // const multer = require("multer"); // Tidak perlu Multer di sini jika hanya untuk middleware global ini
 const cookieParser = require("cookie-parser");
+const swaggerUi = require('swagger-ui-express');
+const fs = require('fs');
+const path = require('path');
 
 const allowedOrigin = process.env.FRONTEND_ORIGIN || "http://localhost:5173";
 
@@ -24,6 +27,9 @@ app.use(cookieParser(process.env.JWT_SECRET_KEY));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
 
+
+const swaggerDocument = JSON.parse(fs.readFileSync(path.join(__dirname, 'docs', 'api-docs.json'), 'utf8'));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/uploads", express.static("public/uploads"));
 
