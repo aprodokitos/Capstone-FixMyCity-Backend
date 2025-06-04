9a17dd9794c1415ea1697d507f2f712b2f4865b0
Torikh <torikhnaser42@gmail.com>
2025-06-04 14:09:16 +0700

    menambahkan input search

--- Routes/index.js
+++ Routes/index.js
@@ -1,13 +1,15 @@
-const userRoutes = require('./user_routes')
-const reportRoutes = require('./report_routes');
-const commentRoutes = require('./comment_routes');
+const userRoutes = require("./user_routes");
+const reportRoutes = require("./report_routes");
+const commentRoutes = require("./comment_routes");
+const inputsearchController = require("./inputsearch_router");
 
-const express = require('express')
+const express = require("express");
 
-const Router = express.Router()
+const Router = express.Router();
 
+Router.use(inputsearchController);
 Router.use(userRoutes);
 Router.use(reportRoutes);
 Router.use(commentRoutes);
 
-module.exports = Router
+module.exports = Router;
