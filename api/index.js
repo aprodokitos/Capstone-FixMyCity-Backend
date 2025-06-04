3401414b059a115328059896676c210715f5e42f
Layalia Nur Afifah <layalia.alia.10@gmail.com>
2025-06-04 22:18:47 +0700

    Test deployment

--- api/index.js
+++ api/index.js
@@ -38,7 +38,7 @@ app.use("/uploads", express.static("public/uploads"));
 app.use(Router); 
 
 app.get("/", (req, res) => {
-    res.send("hello world");
+    res.send("cannot get");
 });
 
 const PORT = process.env.PORT || 3000;
